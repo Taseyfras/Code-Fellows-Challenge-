b185996edf00c58ce46f3b93a408b208b487acdb
Taseyfras <Taseyfras@gmail.com>
2015-10-18 15:51:44 -0700

    updated simple HTTP server for Code Fellows application

--- simpleHTTPserver/simpleHTTPserver.js
+++ simpleHTTPserver/simpleHTTPserver.js
@@ -8,31 +8,40 @@
 //and I completed a node.js tutorial on teamtreehouse.com. I also utilized
 //stackoverflow.com and the node.js documentation.
 
-//requires the http module - modules are like node.js libraries
-var http = require('http');
+//requires the http node module
+var http = require("http");
 //requires POSIX(Portable Operation System Interface) standard input/output functions 
-var fs = require('fs');
+var fs = require("fs");
 
-//fs.readFile(filename[, options], callback) points to the file index.html, checks 
-//for errors, and then either displays an error or logs that program is working 
-fs.readFile('index.html', function (err, html) {
+/*fs.stat checks the url path and then returns either an error or a confirmation that
+the path exists. stats is a node object*/
+fs.stat('index.html', function (err, stats) {
     if (err) {
-        throw err; 
-    } 
-    else {
-    	console.log('index.html is live at port 8080');
-    }      
+        console.log(404 + ' File is unavailable');
+    } else {
+         console.log('File is found');
+             
 
-//http.createServer(request, response) creates a server that responds (and then 
-//serves up that response) upon request
-    http.createServer(function(request, response) {  
-//response.writeHeader gives the status code for the request, in the header
-//and identifies what type of file it is serving(plain text, html, jpg, etc.)
-//response.end lets the server know the message is complete
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
+/*http.createServer(request, response) creates a server that responds (and then 
+serves up that response) upon request*/
+        http.createServer(function(request, response) { 
+/*fs.readFile(filename[, options], callback) points to the file index.html, checks 
+for errors, and then either displays an error or logs that program is working*/ 
+            fs.readFile('index.html', function (err, html) {
+                if (err) {
+                    console.log(500) 
+                } 
+                else {    
+                    console.log('index.html is live at port 8080');
+/*response.writeHeader gives the status code for the request, in the header
+and identifies what type of file it is serving(plain text, html, jpg, etc.)
+response.end lets the server know the message is complete*/
+                    response.writeHeader(200, {'Content-Type': 'text/html'});
+                    response.write(html);     
+                    response.end();  
+                }
  //.listen(port) tells the server to serve index.html to the URL http://localhost:8080 
-    }).listen(8080);
+            });
+        }).listen(8080);
+    }
 });
-
